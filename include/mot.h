b903c8f04ed9a23cebbeef1d3552b8ff08e01dd1
Elsa Navarro <elsa0navarro@gmail.com>
2015-04-06 13:04:22 +0200

    Spécifications de mot.h

--- include/mot.h
+++ include/mot.h
@@ -1,3 +1,9 @@
+/**
+* @file mot.c
+* @author Elsa Navarro & Quentin Dunand
+* @date 04/2015
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -39,10 +45,32 @@ typedef struct dico
 	mot_t* suiv;
 } dico_t;
 
-
+/**
+* Affiche un mot et ses emplacements dans le texte
+* @param m : mot à afficher
+*/
 void print_mot(mot_t m);
 
+/**
+* Crée un mot à partir de son emplacement dans le texte
+* @param word : mot que l'on veut créer (chaine de caractères)
+* @param num_ligne : ligne où se trouve le mot
+* @param num_col : colonne où se trouve le mot
+* @param mot : mot dans lequel on veut créer le mot
+*/
 void create_mot(char* word, int num_ligne, int num_col, mot_t mot);
 
+/**
+* Compare deux mots selon leur ordre alphabétique
+* @param m1 :
+* @param m2 :
+* @return :
+*/
 int compare_mot(mot_t m1, mot_t m2);
+
+/**
+* Insère un mot dans le dictionnaire dans l'ordre alphabtique
+* @param d :
+* @param m : mot à afficher
+*/
 void insertion_dico(dico* d, mot_t m);
